7bbba908403dd9f64aefd69c09f87f11fed3899c
Krasimir Yanakiev <kraskata.qnakiev@gmail.com>
2024-04-06 16:06:40 +0100

    NotAnAssignee attribute created

---
+++ TaskManagementSystem/Attribute/NotAnAssigneeAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+using TaskManagementSystem.Core.Contracts;
+
+namespace TaskManagementSystem.Attribute
+{
+    public class NotAnAssigneeAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            IAssigneeService? assigneeService = context.HttpContext.RequestServices.GetService<IAssigneeService>();
+
+            if (assigneeService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (assigneeService != null && 
+                assigneeService.ExistByIdAsync(context.HttpContext.User.Id()).Result)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+            }
+        }
+    }
+}
