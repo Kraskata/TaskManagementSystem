e523be05ce4e4820bab97cf4432b161ce2adab65
Krasimir Yanakiev <kraskata.qnakiev@gmail.com>
2024-04-06 13:03:31 +0100

    Fixed Index.cshtml showing Category : Category

---
+++ TaskManagementSystem.Core/Services/Assignment/AssignmentService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagementSystem.Core.Contracts.Assignment;
+using TaskManagementSystem.Core.Models.Home;
+using TaskManagementSystem.Infrastructure.Data.Common;
+
+namespace TaskManagementSystem.Core.Services.Assignment
+{
+    public class AssignmentService : IAssignmentService
+    {
+        private readonly IRepository repository;
+
+        public AssignmentService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public async Task<IEnumerable<AssignmentIndexServiceModel>> NewestThreeAssignments()
+        {
+            return await repository
+                .AllReadOnly<Infrastructure.Data.Models.Assignment>()
+                .OrderBy(a => a.Id)
+                .Take(3)
+                .Select(a => new AssignmentIndexServiceModel()
+                {
+                    Id = a.Id,
+                    Title = a.Title,
+                    Description = a.Description,
+                    Category = a.Category
+                })
+                .ToListAsync();
+        }
+    }
+}
