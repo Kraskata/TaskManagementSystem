fd752001f8418645a1ff697be3e75d9944d1c8c7
Krasimir Yanakiev <kraskata.qnakiev@gmail.com>
2024-04-06 13:04:30 +0100

    Revert "Fixed Index.cshtml showing Category : Category"
    This reverts commit e523be05ce4e4820bab97cf4432b161ce2adab65.

--- TaskManagementSystem.Core/Contracts/Assignment/IAssignmentService.cs
+++
@@ -1,9 +0,0 @@
-﻿using TaskManagementSystem.Core.Models.Home;
-
-namespace TaskManagementSystem.Core.Contracts.Assignment
-{
-    public interface IAssignmentService
-    {
-        Task<IEnumerable<AssignmentIndexServiceModel>> NewestThreeAssignments();
-    }
-}
